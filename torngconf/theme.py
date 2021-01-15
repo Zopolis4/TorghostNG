139cec987d4a33fcf016bd3c3a56b980bf1288d4
Git Kern3l <37959590+gitkern3l@users.noreply.github.com>
2021-01-15 18:20:29 +0000

    Add German

--- torngconf/theme.py
+++ torngconf/theme.py
@@ -23,21 +23,25 @@ class English:
     options = "OPTIONS"
     downloading = icon.process + "Downloading {}..."
     installing = icon.process + " Installing {}..."
+    install_pls = icon.error + " Please install {} please"
     uninstalling = icon.process + " Uninstalling TorghostNG..."
     uninstalled = icon.success + " TorghostNG has been uninstalled"
     installed = icon.success + " {} has been installed"
     already_installed = icon.info + " {} is already installed"
-    description = """TorghostNG 1.7 - Make all your internet traffic anonymized through Tor proxy
+    description = """TorghostNG 2.0 - Make all your internet traffic anonymized through Tor proxy
 Rewritten from TorGhost with Python 3"""
     root_please = icon.error + " You must be root, use 'sudo TorghostNG'"
     sorry_windows = icon.error + " Sorry, TorghostNG is not designed for Windows 😛 Use Tor Browser pls"
     sorry_some_os = """I'm sorry, you have to install Tor and macchanger from source by yourself :v I'm too lazy
 Tor: https://github.com/torproject/tor
 macchanger: https://github.com/alobbs/macchanger"""
-    sorry_bsd = "Sorry BSD user, I'm still trying to find way that TorghostNG can fully support for BSD"
+    sorry_bsd = "Sorry BSD user, I'm still trying to find way to fully support for BSD"
     current_language = icon.info + " The current display language: "
-    language_list = icon.info + " List of languages:\n    1.English   2.Vietnamese"
-    choose_your_lang = icon.question + " Choose your language (1/2): "
+    language_list = icon.info + """ List of languages:
+    1. English
+    2. Vietnamese
+    3. German"""
+    choose_your_lang = icon.question + " Choose your language (1-3): "
     wanna_change_lang = icon.question + " Wanna change the display language? (y/n): "
     wanna_uninstall = icon.question + " Wanna uninstall TorghostNG (y/n): "
     invalid_choice = icon.error + " Invalid choice"
@@ -100,11 +104,12 @@ class Vietnamese(English):
     options = "CÁC LỰA CHỌN"
     downloading = icon.process + "Đang tải {}..."
     installing = icon.process + " Đang cài đặt {}..."
+    install_pls = icon.error + " Cài {} đi làm ơn đó ☹"
     uninstalling = icon.process + " Đang gỡ cài đặt TorghostNG..."
     uninstalled = icon.success + " TorghostNG đã được gỡ cài đặt"
     installed = icon.success + " {} đã được cài đặt"
     already_installed = icon.info + " {} đã được cài đặt sẵn"
-    description = """TorghostNG 1.7 - Giúp bạn kết nối Internet ẩn danh qua Tor
+    description = """TorghostNG 2.0 - Giúp bạn kết nối Internet ẩn danh qua Tor
 Được gõ lại từ TorGhost bằng Python 3"""
     root_please = icon.error + " Phải chạy TorghostNG với quyền root nha, thử 'sudo torghostng' xem"
     sorry_windows = icon.error + " Xin lỗi các bạn dùng Windows nhá ☹ Các bạn dùng Tor Brower nha"
@@ -113,8 +118,11 @@ Tor: https://github.com/torproject/tor
 macchanger: https://github.com/alobbs/macchanger"""
     sorry_bsd = "Mình đang tìm các hỗ trợ BSD, xin lỗi bạn :("
     current_language = icon.info + " Ngôn ngữ hiển thị hiện tại: "
-    language_list = icon.info + " Danh sách các ngôn ngữ có sẵn:\n    1.English   2.Vietnamese"
-    choose_your_lang = icon.question + " Chọn ngôn ngữ của bạn (1/2): "
+    language_list = icon.info + """ Danh sách các ngôn ngữ có sẵn:
+    1. Tiếng Anh
+    2. Tiếng Việt
+    3. Tiếng Đức"""
+    choose_your_lang = icon.question + " Chọn ngôn ngữ của bạn (1-3): "
     wanna_change_lang = icon.question + " Muốn thay đổi ngôn ngữ hiển thị không? (y/n): "
     wanna_uninstall = icon.question + " Bạn muốn gỡ TorghostNG đi không (y/n): "
     invalid_choice = icon.error + " Lựa chọn không hợp lệ lmao :v"
@@ -146,7 +154,7 @@ macchanger: https://github.com/alobbs/macchanger"""
     updating = icon.process + " Đang cập nhật TorghostNG lên phiên bản {}..."
     already_configured = icon.info + " Tệp cấu hình {} đã được thiết lập sẵn"
     configuring = icon.process + " Đang thiết lập cấu hình {}..."
-    ipv6_alreay_disabled = icon.info + " IPv6 đã bị vô hiệu hóa sẵn"
+    ipv6_already_disabled = icon.info + " IPv6 đã bị vô hiệu hóa sẵn"
     disabling_ipv6 = icon.process + " Đang vô hiệu hóa IPv6..."
     restoring_configuration = icon.process + " Đang khôi phục thiếp lập {}..."
     stopping_tor = icon.process + " Đang ngưng tiến trình của Tor..."
@@ -171,6 +179,85 @@ macchanger: https://github.com/alobbs/macchanger"""
     dns_tip = icon.info + " Nếu bạn gặp vấn đề với việc phân giải địa chỉ web, dùng '--dns' để sửa"
     interface_error = icon.error + " Không có interface nào tên {}. Thay đổi thất bại"
     video_tutorials = icon.info + " Nếu có thắc mắc gì thì các cậu xem video hướng dẫn nha: "+ color.BOLD +"https://bit.ly/34TNglL"+ color.END
+    
+class German(English):
+    options = "OPTIONEN"
+    downloading = icon.process + "Herunterladen {}..."
+    installing = icon.process + " Installieren {}..."
+    uninstalling = icon.process + " TorghostNG wird deinstalliert..."
+    uninstalled = icon.success + " TorghostNG wurde deinstalliert"
+    installed = icon.success + " {} wurde installiert"
+    already_installed = icon.info + " {} ist schon installiert"
+    description = """TorghostNG 2.0 - Anonymisiere all deinen Internetverkehr druch Tor
+neu programmiert ausgehend von TorGhost mit Python 3"""
+    root_please = icon.error + " Du must root sein, benutze 'sudo TorghostNG'"
+    sorry_windows = icon.error + " Entschuldigung, TorghostNG ist nicht kompatibel mit Windows 😛 Bitte benutze den Tor Browser"
+    sorry_some_os = """Entschuldigung, aber du musst Tor und macchanger von dem Quellcode selber installieren :v Ich bin zu faul dafür
+Tor: https://github.com/torproject/tor
+macchanger: https://github.com/alobbs/macchanger"""
+    sorry_bsd = "Sorry BSD user, I'm still trying to find way that TorghostNG can fully support for BSD"
+    current_language = icon.info + " The current display language: "
+    language_list = icon.info + """ List of languages:
+    1. English
+    2. Vietnamese
+    3. German"""
+    choose_your_lang = icon.question + " Ändere deine Sprache (1-3): "
+    wanna_change_lang = icon.question + " Willst du deine Sprache ändern? (y/n): "
+    wanna_uninstall = icon.question + " Möchtest du Tor Ghost deinstallieren? (y/n): "
+    invalid_choice = icon.error + " Falsche auswahl"
+    country_id = "ISO Abkürzung des Landes"
+    help_help = "Zeige diese hilfe Nachricht und schließen"
+    privoxy_help = "Verbinen zum Tor Netzwerk mit Privoxy - Verbessere deine Privatsphäre"
+    start_help = "Starte die Verbindung zu Tor"
+    stop_help = "Stoppe die Verbindung zu Tor"
+    circuit_help = "Erneuere die aktuelle Tor Verbindung"
+    id_help = "Verbinde dich mit einem Tor-Ausgang in einem spezifischen Land. Gehe zu CountryCode.org und suche den Land ISO code."
+    update_help = "Suche nach neuen Versionen"
+    no_delay_help = "Schalte die Verzögerungszeit aus"
+    changemac_help = "Ändere deine MAC Adresse zufällig. Benutze 'ifconfig' um Geräteinterfaces anzuzeigen"
+    language_help = "Ändere die Anzeigesprache. Englisch ist der Standart"
+    language_list_help = "Zeige mögliche Sprachauswahlen"
+    checkip_help = "Teste deine aktuelle IPV4 Adresse"
+    dns_help = "Benutze diese option um DNS zu reparieren wenn eine Website Adresse nicht aufgelöst werden kann"
+    done = color.GREEN+ " Fertig"+color.END
+    disable_ipv6_info = icon.info + color.BOLD + " Aus Sicherheitsgründen schaltet TorghostNG IPv6 Verkehr aus, um IPv6 Lecks zu verhindern" + color.END
+    iptables_info = icon.info + """ Nicht-Tor Verkehr wird von iptables blockiert.
+    Manche Anwendungen werden sich nicht mit dem Internet verbinden können"""
+    block_bittorrent = icon.info + """ Um das Tor-Netzwerk nicht unnötig zu belasten, wird BitTorrent Verkehr von iptables blockiert."""
+    applying_language = icon.process + " Anzeigesprache wird geändert..."
+    checking_update = icon.process + " Prüfen auf neue Versionen von TorghostNG..."
+    outofdate = icon.error + " Es gibt neue Versionen von TorghostNG"
+    uptodate = icon.success + " Deine Verison von TorghostNG is aktuell"
+    wanna_update = icon.question + " Möchtest du TorghostNG aktualisieren? (y/n): "
+    updating = icon.process + " TorghostNG wird aktualisiert zur Verison {}..."
+    already_configured = icon.info + " {} ist bereits konfiguriert"
+    configuring = icon.process + " Konfigurieren von {} ..."
+    restoring_configuration = icon.process + " Wiederherstellen der Konfiguration {}..."
+    ipv6_already_disabled = icon.info + " IPv6 ist bereits ausgeschaltet"
+    disabling_ipv6 = icon.process + " Ausschalten von IPv6..."
+    stopping_tor = icon.process + " Tor Dienst wird gestoppt..."
+    starting_tor = icon.process + " Tor Dienst wird gestartet..."
+    changing_tor_circuit = icon.process + " Änderung des Tor-Pfades..."
+    setting_iptables = icon.process + " Einstellen der iptables Regeln..."
+    flushing_iptables = icon.process + " Aktualisieren von iptables, zurücksetzen auf Standarteinstellungen..."
+    checking_ip = icon.process + " Testen deiner aktuellen IP..."
+    fixing_dns = icon.process + " Reparieren deines DNS Problems..."
+    your_ip = icon.info + " Deine aktuelle {} Adresse: "
+    checking_tor = icon.process + " Testen deiner Tor Verbindung..."
+    tor_success = icon.success + " Erfolg! Du bist nun zu Tor verbunden {}"
+    tor_failed = icon.error + " Dein Verbindungsprozess zu Tor ist fehlgeschlagen"
+    tor_disconnected = icon.success + " Deine Verbindung zu Tor wurde getrennt"
+    try_again = icon.question + " Möchtest du es erneut probieren (y/n): "
+    restarting_network = icon.process + " Neustarten des NetworkManager..."
+    changing_mac = icon.process + " Änderung deiner aktuellen MAC Adresse..."
+    mac_changed = icon.success + " Deine Mac Adresse wurde geändert"
+    ifconfig_tip = icon.info + color.BOLD + " Du kannst 'ifconfig' benutzen um Netzwerkadapter anzuzeigen" + color.END
+    id_tip = icon.info + color.BOLD + " Du kannst zu https://CountryCode.org gehen um Länder ISO codes zu finden" + color.END
+    torghostng_tip = icon.info + color.BOLD + " Du kannst TorghostNG mit '{}' starten"
+    dns_tip = icon.info + " Wenn du Probleme hast mit DNS, kannst du '--dns' benutzen um das Problem zu lösen"
+    interface_error = icon.error + " Es existiert kein Netzwerkadapter namens {}. Änderung fehlgeschlagen"
+    video_tutorials = icon.info + """ Wenn du Fragen hast, kannst du dir TorghostNG Tutorial Videos hier anschauen: """+ color.BOLD +"""https://bit.ly/34TNglL"""+ color.END +"""
+    Ich denke die sollten dir gefallen:D"""
     
 the_banner = color.GREEN + """ _____               _               _   _   _  ____ 
 |_   _|__  _ __ __ _| |__   ___  ___| |_| \ | |/ ___|
